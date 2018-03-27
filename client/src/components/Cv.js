ebd2604e4bdc7675af7e5a5ebaddc6ec1d77409d
Andrew Locke <alocke12992@gmail.com>
2018-03-27 09:38:09 -0600

    adds CV layout

--- client/src/components/Cv.js
+++ client/src/components/Cv.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Divider, Grid, Header, Segment } from 'semantic-ui-react'
 
 class Cv extends React.Component {
 
   render() {
     return (
-      <div>
-        Cv Page
-      </div>
+      <Grid>
+        <Grid.Row>
+          <Divider />
+        </Grid.Row>
+        <Grid.Row columns={ 2 }>
+          <Grid.Column>
+            <Segment>
+              <Header as='h2' textAlign='center'>Research Interests</Header>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <Header as='h2' textAlign='center'>CURRICULUM VITAE (Sept. 2017)</Header>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
