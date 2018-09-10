feae50d46a5ab01f46b80be51f76cf2b32096eb8
Andrew Locke <alocke12992@gmail.com>
2018-09-09 20:40:19 -0700

    Courses backend

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,39 +1,47 @@
 class Api::CoursesController < ApplicationController
-  before_action :set_university, only: [:show, :update, :destroy]
+  before_action :set_university
+  before_action :set_course, only: [:show, :update, :destroy]
   # before_action :authenticate_user!, only: [:show, :update, :destroy]
 
   def index
-    @university = University.all
-    render json: @university
+    @courses = @university.courses
+    render json: @courses
   end
 
   def create
-    university = University.create(university_params)
-    if university.save
-      render json: university
+    course = @university.courses.create(course_params)
+    if course.save
+      render json: course
     else
-      render json: { errors: university.errors.full_messages.join(',') }, status: 422
+      render json: { errors: course.errors.full_messages.join(',') }, status: 422
     end
   end
 
   def update
-    if @university.update(university_params)
-      render json: @university
+    if @course.update(course_params)
+      render json: @course
     else 
-      render json: {errors: @university.errors.full_messages.join(',') }, status: 422
+      render json: {errors: @course.errors.full_messages.join(',') }, status: 422
     end
   end
 
   def destroy
-    @university.destroy
+    @course.destroy
   end
 
   private
   def set_university
-    @university = University.find(params[:id])
+    @university = University.find(params[:university_id])
   end
 
-  def university_params
-    params.require(:university).permit(:name)
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(
+      :title,
+      :syllabus
+    )
   end
 end
